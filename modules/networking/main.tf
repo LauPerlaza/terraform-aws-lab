8483c906bd0735f76c105ad98af0b83e61f5064a
laura perlaza <117466164+LauPerlaza@users.noreply.github.com>
2023-05-23 22:14:54 -0500

    creacion de modules (#2)

---
+++ modules/networking/main.tf
@@ -0,0 +1,61 @@
+#   #   # AWS_VPC #  #   #
+resource "aws_vpc" "vpc_test" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+  tags = {
+    Name        = "vpc_${var.environment}"
+    Environment = "${var.environment}"
+  }
+}
+#   #   # AWS_SUBNETS_PUBLIC #  #   #
+resource "aws_subnet" "sub_public1" {
+  vpc_id            = aws_vpc.vpc_test.id
+  cidr_block        = "10.0.0.0/24"
+  availability_zone = "${var.region}a"
+  tags = {
+    Name = "sub_public1_${var.environment}"
+  }
+}
+resource "aws_subnet" "sub_public2" {
+  vpc_id            = aws_vpc.vpc_test.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "${var.region}b"
+  tags = {
+    Name = "sub_public2_${var.environment}"
+  }
+}
+#   #   # AWS_SUBNETS_PUBLIC_db #  #   #
+resource "aws_subnet" "sub_public3_db" {
+  vpc_id            = aws_vpc.vpc_test.id
+  cidr_block        = "10.0.4.0/24"
+  availability_zone = "${var.region}a"
+  tags = {
+    Name = "sub_public3_db_${var.environment}"
+  }
+}
+resource "aws_subnet" "sub_public4_db" {
+  vpc_id            = aws_vpc.vpc_test.id
+  cidr_block        = "10.0.5.0/24"
+  availability_zone = "${var.region}b"
+  tags = {
+    Name = "sub_public4_db_${var.environment}"
+  }
+}
+#   #   # AWS_SUBNETS_PRIVATE #  #   #
+resource "aws_subnet" "sub_private1" {
+  vpc_id            = aws_vpc.vpc_test.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "${var.region}a"
+  tags = {
+    Name = "sub_private1_${var.environment}"
+  }
+}
+resource "aws_subnet" "sub_private2" {
+  vpc_id            = aws_vpc.vpc_test.id
+  cidr_block        = "10.0.3.0/24"
+  availability_zone = "${var.region}b"
+  tags = {
+    Name = "sub_private2_${var.environment}"
+  }
+}
