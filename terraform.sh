4c79986853d8e05fcaf4cdd1000ec92cca050645
laura perlaza <117466164+LauPerlaza@users.noreply.github.com>
2023-05-31 12:24:56 -0500

    script de bash

---
+++ terraform.sh
@@ -0,0 +1,20 @@
+# ! bin/bash 
+#Programa para correr los comandos de terraform 
+
+#ruta al directorio del proyecto de terraform 
+
+terraform_dir="C:\Users\Laura Perlaza\Documents\terraform-aws-lab"
+
+#cambia al directorio del proyecto de terraform 
+
+cd "terraform_dir"
+
+#iniciar el dorectorio del proyecto de terraform 
+terraform init
+
+echo "acaba de iniciar el dirextorio de proyecto de terraform"
+
+#ejecutar el plan de terraform
+terraform plan
+
+echo "acaba de ejecutar el plan de terraform"
