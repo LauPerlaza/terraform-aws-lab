abefaa4cf44a80dd9cc9e1a9bedc0b71be87e2a4
laura perlaza <117466164+LauPerlaza@users.noreply.github.com>
2023-05-31 23:59:38 -0500

    documentación del codigo

--- data.tf
+++ data.tf
@@ -1,13 +1,21 @@
+# Este bloque se utiliza para definir un documento de política de IAM 
+#que no se va a crear ni modificar, sino que se utilizará como datos en otros recursos.
 data "aws_iam_policy_document" "s3_policy" {
   statement {
+    #se especifica el tipo de entidad principal a la que se aplicará la política. 
+    #En este caso, el tipo es "AWS" y se proporciona el ARN (Amazon Resource Name) del usuario "laura.perlaza"
     principals {
       type        = "AWS"
       identifiers = ["arn:aws:iam::017333715993:user/laura.perlaza"]
     }
+    # actions contiene una lista de acciones (permisos) que se permitirán en la política. 
+    #En este caso, se permiten las acciones "s3:GetObject" y "s3:ListBucket".
     actions = [
       "s3:GetObject",
       "s3:ListBucket",
     ]
+    #El atributo resources define los recursos a los que se aplicarán las acciones permitidas.
+    #Aquí se utiliza el comodín ("*"), lo que indica que las acciones se aplicarán a cualquier recurso de S3.
     resources = [
       "*"
     ]
--- modules/s3_bucket/output.tf
+++ modules/s3_bucket/output.tf
@@ -1,6 +1,10 @@
+#Esta salida muestra el ARN del bucket de S3 creado o
+#modificado por el recurso aws_s3_bucket.s3_test
 output "bucket_arn" {
   value = aws_s3_bucket.s3_test.arn
 }
+#Esta salida muestra el ID del bucket de S3 creado o 
+#modificado por el recurso aws_s3_bucket.s3_test
 output "bucket_id" {
   value = aws_s3_bucket.s3_test.id
 }
