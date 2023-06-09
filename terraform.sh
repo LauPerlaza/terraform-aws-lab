c53dc30a18faaf7b534d98bc070fd9382011f24f
laura perlaza <117466164+LauPerlaza@users.noreply.github.com>
2023-06-08 21:11:45 -0500

    mejoras al bash scrpit que se llama terraform.sh

--- terraform.sh
+++ terraform.sh
@@ -1,21 +1,27 @@
-# ! bin/bash 
-#Programa para correr los comandos de terraform 
+#!/bin/bash
 
-#ruta al directorio del proyecto de terraform 
+#se verifica la cantidad de argumentos que se pasan al script utilizando $#, que representa el número de argumentos. Si la cantidad de argumentos es diferente de 2, se muestra un mensaje de uso y se sale del script con exit 1 para indicar un error.
 
-terraform_dir="C:/Users/Laura Perlaza/Documents/terraform-aws-lab"
+if [ $# -ne 2 ]; then
+  echo "Uso: ./script <región> <entorno>"
+  exit 1
+fi
 
-#cambia al directorio del proyecto de terraform 
+# Asignar los argumentos a variables
+region=$1
+environment=$2
 
-cd "terraform_dir"
 
-#iniciar el dorectorio del proyecto de terraform 
-terraform init
+#La opción -backend-config se utiliza para proporcionar configuraciones específicas del backend de Terraform durante la inicialización del directorio de trabajo de Terraform.
+# Iniciar el directorio del proyecto de Terraform con las variables de región y entorno
+echo "Iniciando el directorio del proyecto de Terraform con la región '$region' y el entorno '$environment'..."
+terraform init -backend-config="region=$region" -backend-config="environment=$environment"
+echo "Se ha iniciado el directorio del proyecto de Terraform."
 
-echo "acaba de iniciar el directorio de proyecto de terraform"
-
-#ejecutar el plan de terraform
-terraform plan
-
-echo "acaba de ejecutar el plan de terraform"
+# Ejecutar el plan de Terraform con las variables de región y entorno
+echo "Ejecutando el plan de Terraform con los siguientes valores:"
+echo "Región: $region"
+echo "Entorno: $environment"
+terraform plan -var "region=$region" -var "environment=$environment"
+echo "Se ha ejecutado el plan de Terraform."
 
