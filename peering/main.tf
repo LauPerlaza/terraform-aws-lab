98c5c6282ee885220c82280f13586a76544abfe9
laura perlaza <117466164+LauPerlaza@users.noreply.github.com>
2023-06-09 23:28:59 -0500

    mejoras al main de peering

--- peering/main.tf
+++ peering/main.tf
@@ -2,7 +2,6 @@ module "vpc1" {
   source                      = "../modules/networking"
   region                      = var.region
   environment                 = var.environment
-  name_vpc                    = "vpc1"
   ip                          = "181.63.51.122/32"
   cidr_block_vpc              = "10.20.0.0/16"
   cidr_block_subnet_public    = ["10.20.1.0/24", "10.20.2.0/24"]
@@ -14,7 +13,6 @@ module "vpc2" {
   source                      = "../modules/networking"
   region                      = var.region
   environment                 = var.environment
-  name_vpc                    = "vpc2"
   ip                          = "181.63.51.122/32"
   cidr_block_vpc              = "192.168.0.0/16"
   cidr_block_subnet_public    = ["192.168.10.0/24", "192.168.20.0/24"]
@@ -23,17 +21,17 @@ module "vpc2" {
 }
 
 resource "aws_vpc_peering_connection" "peering_connection" {
-  vpc_id      = module.vpc1.id
-  peer_vpc_id = module.vpc2.id
+  vpc_id      = module.vpc1.vpc_id
+  peer_vpc_id = module.vpc2.vpc_id
   auto_accept = true
 }
 
 resource "aws_route_table" "route_table_vpc1" {
-  vpc_id = module.vpc1.id
+  vpc_id = module.vpc1.vpc_id
 }
 
 resource "aws_route_table" "route_table_vpc2" {
-  vpc_id = module.vpc2.id
+  vpc_id = module.vpc2.vpc_id
 }
 
 resource "aws_route" "route_vpc2" {
@@ -72,8 +70,8 @@ resource "aws_security_group" "sgroup_ec2" {
   vpc_id      = module.vpc1.id
 
   ingress {
-    from_port   = 0
-    to_port     = 0
+    from_port   = -1
+    to_port     = -1
     protocol    = "icmp"
     cidr_blocks = ["0.0.0.0/0"]
   }
@@ -93,6 +91,3 @@ resource "null_resource" "ping_ec2" {
     command = "ping -c 4 ${module.ec2_2.private_ip}"
   }
 }
-
-
-
