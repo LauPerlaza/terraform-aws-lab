3627a64db2e6ca4ee56ed8f99f9d035b22f35490
laura perlaza <117466164+LauPerlaza@users.noreply.github.com>
2023-07-18 12:27:43 -0500

    add automate-terraform prueba 23

--- variables.tf
+++ variables.tf
@@ -10,6 +10,7 @@ variable "region" {
 #que se están desplegando los recursos de infraestructura.
 variable "environment" {
   type = string
+  default   = "test"
 }
 #Esta variable se utiliza para especificar si se debe utilizar el servicio de KMS
 #para el cifrado del bucket de S3. El tipo de variable se establece en bool, 
