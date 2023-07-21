b74e09aa180e30b3ed7a490eff7ac2211ae06f80
laura perlaza <117466164+LauPerlaza@users.noreply.github.com>
2023-07-21 17:51:21 -0500

    agregue variables

--- backend.tf
+++ backend.tf
@@ -1,8 +1,3 @@
 terraform {
-  backend "s3" {
-    bucket         = "test-terraf-state-lab"
-    dynamodb_table = "table-terraform-tf"
-    key            = "terraform-tfstate"
-    region         = "us-east-1"
-  }
+  backend "s3" { }
 }
