4963e62a6bf5972e6aaaa4e4daaf7995f5ab3d50
laura perlaza <117466164+LauPerlaza@users.noreply.github.com>
2023-07-21 18:36:30 -0500

    prueba 2

--- .github/jobs/create_resources.sh
+++ .github/jobs/create_resources.sh
@@ -1,32 +1,28 @@
 #!/bin/bash
 
-# Configurar variables.
-
-bucket_name="test-terraf-state-lab"
-table_name="table-terraform-tf"
-region="us-east-1"
+# Configurar variables
 
 # Verificar si el bucket ya existe
-bucket_exists="$(aws s3api head-bucket --bucket "$bucket_name" --region "$region" 2>&1)" 
+bucket_exists="$(aws s3api head-bucket --bucket "${BUCKET_NAME}-${ENVIRONMENT}" --region "$AWS_REGION" 2>&1)" 
 echo $bucket_exists 
 if [[ $bucket_exists == *"Not Found"* ]]; then
 
 # El bucket no existe, crearlo
-  aws s3api create-bucket --bucket "$bucket_name" --region "$region"
-  aws s3api put-bucket-versioning --bucket "$bucket_name" --region "$region" --versioning-configuration Status=Enabled
-  echo "Bucket creado: $bucket_name"
+  aws s3api create-bucket --bucket "${BUCKET_NAME}-${ENVIRONMENT}" --region "$AWS_REGION"
+  aws s3api put-bucket-versioning --bucket "${BUCKET_NAME}-${ENVIRONMENT}" --region "$AWS_REGION" --versioning-configuration Status=Enabled
+  echo "Bucket creado: ${BUCKET_NAME}-${ENVIRONMENT}"
 else
-  echo "El bucket $bucket_name ya existe."
+  echo "El bucket ${BUCKET_NAME}-${ENVIRONMENT} ya existe."
 fi
 
 # Verificar si la tabla ya existe
-table_exists="$(aws dynamodb describe-table --table-name "$table_name" --region "$region" 2>&1)"
+table_exists="$(aws dynamodb describe-table --table-name "${DYNAMODB_TABLE}-${ENVIRONMENT}" --region "$AWS_REGION" 2>&1)"
 echo $table_exists
 if [[ $table_exists == *"Requested resource not found"* ]]; then
 
 # La tabla no existe, crearla
-  aws dynamodb create-table --table-name "$table_name" --region "$region" --attribute-definitions AttributeName=LockID,AttributeType=S --key-schema AttributeName=LockID,KeyType=HASH --billing-mode PAY_PER_REQUEST
-  echo "Tabla creada: $table_name"
+  aws dynamodb create-table --table-name "${DYNAMODB_TABLE}-${ENVIRONMENT}" --region "$AWS_REGION" --attribute-definitions AttributeName=LockID,AttributeType=S --key-schema AttributeName=LockID,KeyType=HASH --billing-mode PAY_PER_REQUEST
+  echo "Tabla creada: ${DYNAMODB_TABLE}-${ENVIRONMENT}"
 else
-  echo "La tabla $table_name ya existe."
+  echo "La tabla ${DYNAMODB_TABLE}-${ENVIRONMENT} ya existe."
 fi
